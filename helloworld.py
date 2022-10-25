f57dca3d23f04fc78037de4f96a6da5a27df5fcf
AntonioTroncoso <101261026+AntonioTroncoso@users.noreply.github.com>
2022-10-25 13:09:11 +0200

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+print('Antonio')
+print('Troncos Barrera')
+print('Jerez')
+print('I am enjoying 30 days of python')
+
+print(type(10))
+print(type(3.14))
+print(type(1 + 3j))
+print(type('Asabeneh'))
+print(type([1, 2, 3]))
+print(type({'name' : 'Asabeneh'}))
+print(type({9.8, 3.14, 2.7}))
+print(type((9.8, 3.14, 2.7)))
+#2
